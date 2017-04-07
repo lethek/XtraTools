b5fe63d8a217fabda270244226b38a9bf3f7152c
Michael Monsour <michael@lethek.net>
2017-04-07 15:30:23 +1000

    created a new custom MSBuild task which dynamically generates a config class at compile-time (based on an XML config file) using CodeDOM and injects it into the build process

---
+++ AirtimeBuildTasks.Tests/InjectConfigTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AirtimeBuildTasks.Tests
+{
+
+	public class InjectConfigTests
+	{
+
+		public InjectConfigTests(ITestOutputHelper output)
+		{
+			Output = output;
+		}
+
+
+		[StaFact]
+		private void ShouldGenerateBuildableCode()
+		{
+			bool success = CallMsBuild(ProjectXml, out string output);
+			Output.WriteLine(output);
+			Assert.True(success);
+		}
+
+
+		private bool CallMsBuild(string buildProject, out string output)
+		{
+			//TODO: refer to this for future updates: http://stackoverflow.com/a/7854806/84899
+			using (var collection = new ProjectCollection()) {
+				var project = collection.LoadProject(XmlReader.Create(new StringReader(buildProject)));
+				var outputBuilder = new StringBuilder();
+				using (var writer = new StringWriter(outputBuilder)) {
+					try {
+						collection.RegisterLogger(new ConsoleLogger(LoggerVerbosity.Normal, x => writer.Write(x), null, null));
+						bool result = project.Build();
+						return result;
+					} finally {
+						output = outputBuilder.ToString();
+						collection.UnregisterAllLoggers();
+					}
+				}
+			}
+		}
+
+
+		private readonly ITestOutputHelper Output;
+
+
+		private const string ProjectXml =
+@"<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+	<UsingTask AssemblyFile=""AirtimeBuildTasks.dll"" TaskName=""InjectConfigTask"" />
+	<PropertyGroup>
+		<IntermediateOutputPath Condition=""$(IntermediateOutputPath) == '' Or $(IntermediateOutputPath) == '*Undefined*'"">$(MSBuildProjectDirectory)\obj\$(Configuration)</IntermediateOutputPath>
+	</PropertyGroup>
+	<Target Name=""UnitTests"" BeforeTargets=""BeforeBuild;BeforeRebuild"">
+		<InjectConfigTask Source=""Settings.config"" Namespace=""Airtime"" OutputPath=""$(IntermediateOutputPath)"">
+			<Output ItemName=""Generated"" TaskParameter=""GeneratedConfigPath"" />
+		</InjectConfigTask>
+		<ItemGroup>
+			<Compile Include=""@(Generated)""/>
+			<FileWrites Include=""@(Generated)""/>
+		</ItemGroup>
+	</Target>
+</Project>";
+
+	}
+
+}
---
+++ AirtimeBuildTasks/InjectConfigTask.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.CSharp;
+
+
+namespace AirtimeBuildTasks
+{
+
+	public class InjectConfigTask : Task
+	{
+
+		[Required]
+		public string Source { get; set; }
+
+		[Required]
+		public string Namespace { get; set; }
+
+		public string Class { get; set; } = "Config";
+
+		[Required]
+		public string OutputPath { get; set; }
+
+		[Output]
+		public string GeneratedConfigPath { get; set; }
+
+
+		public override bool Execute()
+		{
+			try {
+				if (String.IsNullOrEmpty(Source)) {
+					return true;
+				}
+
+				if (!File.Exists(Source)) {
+					throw new Exception($"Could not find source \"{Source}\"");
+				}
+
+				var data = XDocument
+					.Load(Source)
+					.XPathSelectElements("/appSettings/add")
+					.ToDictionary(x => x.Attribute("key")?.Value, x => x.Attribute("value")?.Value)
+					.ToList();
+
+				string code = GenerateConfigCode(Namespace, "Config", data);
+
+				string tempFilePath = OutputPath == null
+					? Path.Combine(Path.GetTempPath(), $"{Namespace.Replace(".", "_")}_{Class}_{Path.GetRandomFileName().Replace(".", "")}.g.cs")
+					: Path.Combine(OutputPath, $"{Namespace.Replace(".", "_")}_{Class}.g.cs");
+
+				string tempFileDir = Path.GetDirectoryName(tempFilePath);
+
+				if (tempFileDir != null) {
+					Directory.CreateDirectory(tempFileDir);
+				}
+
+				File.WriteAllText(tempFilePath, code, Encoding.UTF8);
+
+				GeneratedConfigPath = tempFilePath;
+				return true;
+
+			} catch (Exception ex) {
+				Log.LogErrorFromException(ex);
+				return false;
+			}
+		}
+
+
+		private static string GenerateConfigCode(string namespaceName, string className, IEnumerable<KeyValuePair<string, string>> settings)
+		{
+			var kTypeParam = new CodeTypeParameter("TKey");
+			var vTypeParam = new CodeTypeParameter("TValue");
+			var tTypeParam = new CodeTypeParameter("T");
+
+			var tType = new CodeTypeReference(tTypeParam);
+			var stringType = new CodeTypeReference(typeof(String));
+			var propertyInfoType = new CodeTypeReference(typeof(PropertyInfo));
+			var stringIndexerType = new CodeTypeReference("IIndexer", stringType, stringType);
+			var configIndexerType = new CodeTypeReference("ConfigIndexer<" + className + ">");
+
+			var bindingFlags = new CodeTypeReferenceExpression(typeof(BindingFlags));
+			var configManager = new CodeTypeReferenceExpression("Microsoft.Azure.CloudConfigurationManager");
+			var thisProperties = new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), "_properties");
+			var index = new CodeVariableReferenceExpression("index");
+			var props = new CodeVariableReferenceExpression("props");
+			var prop = new CodeVariableReferenceExpression("prop");
+			var result = new CodeVariableReferenceExpression("result");
+
+
+			var indexerInterface = new CodeTypeDeclaration("IIndexer") {
+				TypeAttributes = TypeAttributes.Public | TypeAttributes.Interface,
+				TypeParameters = { kTypeParam, vTypeParam },
+				Members = {
+					new CodeMemberProperty {
+						Name = "Item",
+						Type = new CodeTypeReference(vTypeParam),
+						HasGet = true,
+						Parameters = {new CodeParameterDeclarationExpression(new CodeTypeReference(kTypeParam), "key")}
+					}
+				}
+			};
+
+			var configIndexerConstructor = new CodeConstructor {
+				Attributes = MemberAttributes.Assembly,
+				Statements = {
+					new CodeVariableDeclarationStatement(typeof(PropertyInfo[]), "props",
+						new CodeMethodInvokeExpression(
+							new CodeTypeOfExpression(tType),
+							"GetProperties",
+							new CodeBinaryOperatorExpression(
+								new CodeFieldReferenceExpression(bindingFlags, "Public"),
+								CodeBinaryOperatorType.BitwiseOr,
+								new CodeFieldReferenceExpression(bindingFlags, "Static")
+							)
+						)
+					),
+					new CodeIterationStatement(
+						new CodeVariableDeclarationStatement(typeof(int), "index", new CodePrimitiveExpression(0)),
+						new CodeBinaryOperatorExpression(
+							index,
+							CodeBinaryOperatorType.LessThan,
+							new CodeFieldReferenceExpression(props, "Length")
+						),
+						new CodeAssignStatement(
+							index,
+							new CodeBinaryOperatorExpression(index, CodeBinaryOperatorType.Add, new CodePrimitiveExpression(1))
+						)
+					) {
+						Statements = {
+							new CodeVariableDeclarationStatement(typeof(PropertyInfo), "prop", new CodeArrayIndexerExpression(props, index)),
+							new CodeMethodInvokeExpression(
+								thisProperties,
+								"Add",
+								new CodePropertyReferenceExpression(prop, "Name"),
+								prop
+							)
+						}
+					}
+				}
+			};
+
+			var configIndexerClass = new CodeTypeDeclaration("ConfigIndexer") {
+				TypeAttributes = TypeAttributes.NestedPrivate,
+				TypeParameters = { tTypeParam },
+				BaseTypes = { stringIndexerType },
+				Members = {
+					configIndexerConstructor,
+					new CodeMemberField(typeof(IDictionary<string,PropertyInfo>), "_properties") {
+						Attributes = MemberAttributes.Private,
+						InitExpression = new CodeObjectCreateExpression(typeof(Dictionary<string, PropertyInfo>))
+					},
+					new CodeMemberProperty {
+						Attributes = MemberAttributes.Public | MemberAttributes.Final,
+						Name = "Item",
+						Type = stringType,
+						Parameters = {new CodeParameterDeclarationExpression(stringType, "key")},
+						GetStatements = {
+							new CodeVariableDeclarationStatement(
+								stringType,
+								result.VariableName,
+								new CodeMethodInvokeExpression(configManager, "GetSetting", new CodeArgumentReferenceExpression("key"))
+							),
+							new CodeConditionStatement(
+								new CodeBinaryOperatorExpression(
+									result,
+									CodeBinaryOperatorType.IdentityInequality,
+									new CodePrimitiveExpression(null)
+								),
+								new CodeMethodReturnStatement(result)
+							),
+							new CodeVariableDeclarationStatement(propertyInfoType, "prop"),
+							new CodeConditionStatement(
+								new CodeMethodInvokeExpression(
+									thisProperties,
+									"TryGetValue",
+									new CodeArgumentReferenceExpression("key"),
+									new CodeDirectionExpression(FieldDirection.Out, prop)
+								),
+								new CodeMethodReturnStatement(
+									new CodeMethodInvokeExpression(
+										new CodeMethodInvokeExpression(prop, "GetValue", new CodeTypeOfExpression(tType)),
+										"ToString"
+									)
+								)
+							),
+							new CodeMethodReturnStatement(new CodePrimitiveExpression(null))
+						}
+					}
+				}
+			};
+
+			var members = settings
+				.Select(
+					setting => new CodeMemberProperty {
+						Attributes = MemberAttributes.Public | MemberAttributes.Static,
+						Type = stringType,
+						Name = setting.Key,
+						GetStatements = {
+							new CodeVariableDeclarationStatement(
+								stringType,
+								result.VariableName,
+								new CodeMethodInvokeExpression(configManager, "GetSetting", new CodePrimitiveExpression(setting.Key))
+							),
+							new CodeConditionStatement(
+								new CodeBinaryOperatorExpression(
+									new CodeVariableReferenceExpression(result.VariableName),
+									CodeBinaryOperatorType.IdentityInequality,
+									new CodePrimitiveExpression(null)
+								),
+								new CodeMethodReturnStatement(result)
+							),
+							new CodeMethodReturnStatement(new CodePrimitiveExpression(setting.Value))
+						}
+					}
+				)
+				.Cast<CodeTypeMember>()
+				.ToArray();
+
+			var configClass = new CodeTypeDeclaration(className) {
+				TypeAttributes = TypeAttributes.Public | TypeAttributes.Sealed,
+				CustomAttributes = {
+					new CodeAttributeDeclaration(
+						new CodeTypeReference(typeof(CompilerGeneratedAttribute), CodeTypeReferenceOptions.GlobalReference)
+					)
+				},
+				Members = {
+					new CodeConstructor { Attributes = MemberAttributes.Private },
+					new CodeMemberField {
+						Attributes = MemberAttributes.Private | MemberAttributes.Static,
+						Name = "_items",
+						Type = stringIndexerType,
+						InitExpression = new CodeObjectCreateExpression(configIndexerType)
+					},
+					new CodeMemberProperty {
+						Attributes = MemberAttributes.Public | MemberAttributes.Static,
+						Name = "Items",
+						Type = stringIndexerType,
+						GetStatements = {
+							new CodeMethodReturnStatement(new CodeFieldReferenceExpression(null, "_items"))
+						}
+					},
+					indexerInterface,
+					configIndexerClass,
+				}
+			};
+			configClass.Members.AddRange(members);
+
+			var configNamespace = new CodeNamespace(namespaceName);
+			configNamespace.Types.Add(configClass);
+
+			var compileUnit = new CodeCompileUnit();
+			compileUnit.Namespaces.Add(configNamespace);
+
+			using (var codeProvider = new CSharpCodeProvider())
+			using (var writer = new StringWriter()) {
+				codeProvider.GenerateCodeFromCompileUnit(compileUnit, writer, new CodeGeneratorOptions { BlankLinesBetweenMembers = false });
+				return writer.ToString();
+			}
+
+		}
+
+	}
+
+}
